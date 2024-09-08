9b656b44fd79a7f6b8b5be2041bfa6a8f19df486
Kevin K H Zheng <kevin.k.h.zheng@qq.com>
2024-09-08 11:49:22 +0800

    支持多标签

--- typora/src/main/java/com/example/typora/Main.java
+++ typora/src/main/java/com/example/typora/Main.java
@@ -12,12 +12,20 @@ import com.vladsch.flexmark.util.data.MutableDataSet;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-        MarkdownEditor editor = new MarkdownEditor();
+        MarkdownEditor editor = new MarkdownEditor(primaryStage);
         Scene scene = new Scene(editor, 800, 600);
+        scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
         primaryStage.setTitle("Typora-like Markdown Editor");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -28,23 +36,18 @@ public class Main extends Application {
     }
 
     class MarkdownEditor extends BorderPane {
-        private TextArea markdownArea;
-        private WebView preview;
+        private TabPane tabPane;
         private MenuBar menuBar;
+        private Stage primaryStage;
     
-        public MarkdownEditor() {
-            markdownArea = new TextArea();
-            preview = new WebView();
-    
-            markdownArea.textProperty().addListener((observable, oldValue, newValue) -> {
-                updatePreview(newValue);
-            });
-    
+        public MarkdownEditor(Stage stage) {
+            this.primaryStage = stage;
+            tabPane = new TabPane();
+            
             createMenuBar();
     
             setTop(menuBar);
-            setLeft(markdownArea);
-            setCenter(preview);
+            setCenter(tabPane);
         }
     
         private void createMenuBar() {
@@ -58,16 +61,60 @@ public class Main extends Application {
     
             menuBar.getMenus().addAll(fileMenu, editMenu, formatMenu, viewMenu, helpMenu);
     
-            // Add menu items to each menu as needed
-            // For example:
-            fileMenu.getItems().addAll(new MenuItem("新建"), new MenuItem("打开"), new MenuItem("保存"));
+            MenuItem openMenuItem = new MenuItem("打开");
+            MenuItem saveMenuItem = new MenuItem("保存");
+            
+            openMenuItem.setOnAction(event -> openFile());
+            
+            fileMenu.getItems().addAll(openMenuItem, saveMenuItem);
             editMenu.getItems().addAll(new MenuItem("撤销"), new MenuItem("重做"), new MenuItem("剪切"), new MenuItem("复制"), new MenuItem("粘贴"));
             formatMenu.getItems().addAll(new MenuItem("加粗"), new MenuItem("斜体"), new MenuItem("下划线"));
             viewMenu.getItems().addAll(new MenuItem("源代码模式"), new MenuItem("预览模式"));
             helpMenu.getItems().addAll(new MenuItem("关于"), new MenuItem("帮助文档"));
         }
     
-        private void updatePreview(String markdown) {
+        private void openFile() {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("打开文件");
+            fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Markdown Files", "*.md"),
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                new FileChooser.ExtensionFilter("All Files", "*.*")
+            );
+            File selectedFile = fileChooser.showOpenDialog(primaryStage);
+            if (selectedFile != null) {
+                try {
+                    String content = new String(Files.readAllBytes(Paths.get(selectedFile.getPath())));
+                    createNewTab(selectedFile.getName(), content);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    // 在这里可以添加错误处理，比如显示一个错误对话框
+                }
+            }
+        }
+        
+        private void createNewTab(String title, String content) {
+            Tab tab = new Tab(title);
+            BorderPane tabContent = new BorderPane();
+            
+            TextArea markdownArea = new TextArea(content);
+            WebView preview = new WebView();
+            
+            markdownArea.textProperty().addListener((observable, oldValue, newValue) -> {
+                updatePreview(newValue, preview);
+            });
+            
+            tabContent.setLeft(markdownArea);
+            tabContent.setCenter(preview);
+            
+            tab.setContent(tabContent);
+            tabPane.getTabs().add(tab);
+            tabPane.getSelectionModel().select(tab);
+            
+            updatePreview(content, preview);
+        }
+        
+        private void updatePreview(String markdown, WebView preview) {
             MutableDataSet options = new MutableDataSet();
             Parser parser = Parser.builder(options).build();
             HtmlRenderer renderer = HtmlRenderer.builder(options).build();
