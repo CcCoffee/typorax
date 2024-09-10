59230c4cbfe1c9cf06dc39c08245d67dfd97290c
Kevin K H Zheng <kevin.k.h.zheng@qq.com>
2024-09-10 23:57:27 +0800

    session.ser放在.typorax目录下

--- src/main/java/com/example/typorax/manager/SessionManager.java
+++ src/main/java/com/example/typorax/manager/SessionManager.java
@@ -3,14 +3,14 @@ package com.example.typorax.manager;
 import com.example.typorax.component.CustomTabPane;
 import com.example.typorax.model.TabInfo;
 import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SessionManager {
-    private static final String SESSION_FILE = "session.ser";
+    private static final String USER_CONFIG_DIR = System.getProperty("user.home") + File.separator + ".typorax";
+    private static final String SESSION_FILE = USER_CONFIG_DIR + File.separator + "session.ser";
 
     public static void saveSession(List<TabInfo> tabs) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(SESSION_FILE))) {
